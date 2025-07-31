17a9b23bd7e27e8f0a1d3e86518ab93d359f911d
Stanley Xu <stanley.xuu@gmail.com>
2025-07-30 21:04:21 -0400

    Adjust env

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ import createMDX from '@next/mdx'
 const nextConfig: NextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
+  // For gh-pages deployment
+  output: 'export',
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 const withMDX = createMDX({
