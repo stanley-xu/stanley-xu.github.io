96a46ebfd1828fa8405dcb1651e7474c46aa5eda
Stanley Xu <stanley.xuu@gmail.com>
2025-07-30 21:59:58 -0400

    Add /art page, fix breadcrumb spacing

---
+++ src/app/art/page.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import profilePic from "./avatar-800x800.jpeg";
+import { TextLayout } from "@/lib/TextLayout";
+import Content from "@/content/about-art.md";
+
+export default function Page() {
+  return (
+    <>
+      <header
+        style={{
+          padding: "32px 0",
+          marginBottom: "64px",
+          borderBottom: "2px solid hsl(0deg, 0%, 20%)",
+        }}
+      >
+        <div
+          style={{
+            margin: "0 auto",
+            width: "fit-content",
+          }}
+        >
+          <Image
+            src={profilePic}
+            alt="Picture of the author"
+            className="w-16"
+            width={500} // automatically provided
+            height={500} // automatically provided
+            blurDataURL="data:..." // automatically provided
+            placeholder="blur" // Optional blur-up while loading
+          />
+        </div>
+      </header>
+      <main>
+        <TextLayout>
+          <Content />
+        </TextLayout>
+      </main>
+    </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "./ui/header";
+import Header from "./ui/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
