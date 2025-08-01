5be2eac968e2a50c815fe96c2e51223c0e5e5595
Stanley Xu <stanley.xuu@gmail.com>
2025-07-31 23:27:34 -0400

    fix: image aspect ratios

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -22,9 +22,7 @@ export default function Page() {
           <Image
             src={profilePic}
             alt="Picture of the author"
-            className="w-16"
-            width={500} // automatically provided
-            height={500} // automatically provided
+            width={300} // Not setting height to maintain aspect ratio
             blurDataURL="data:..." // automatically provided
             placeholder="blur" // Optional blur-up while loading
           />
--- src/app/art/page.tsx
+++ src/app/art/page.tsx
@@ -22,7 +22,6 @@ export default function Page() {
           <Image
             src={profilePic}
             alt="Picture of the author"
-            style={{ width: '4rem', height: '4rem' }}
             width={500} // automatically provided
             height={500} // automatically provided
             blurDataURL="data:..." // automatically provided
