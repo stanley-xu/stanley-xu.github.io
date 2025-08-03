82dca8ab3136bf8f940a6453fc2b4fa7bba8e8c7
Stanley Xu <stanley.xuu@gmail.com>
2025-08-02 21:27:00 -0400

    Simplify tokens

--- src/app/components/TextLayout.tsx
+++ src/app/components/TextLayout.tsx
@@ -2,11 +2,12 @@ import { ReactNode } from "react";
 
 export function TextLayout({ children }: { children: ReactNode }) {
   return (
-    <div style={{ 
-      maxWidth: '65ch', 
-      lineHeight: 1.6, 
-      color: 'var(--foreground)' 
-    }}>
+    <div
+      style={{
+        maxWidth: "65ch",
+        lineHeight: 1.6,
+      }}
+    >
       {children}
     </div>
   );
